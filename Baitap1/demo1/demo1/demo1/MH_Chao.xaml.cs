939e82d9f670327270119fd26e861eacafc98cfe
DinhIT98 <ndinh725@gmail.com>
2019-09-29 14:25:02 +0700

    version 1

---
+++ Baitap1/demo1/demo1/demo1/MH_Chao.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace demo1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MH_Chao : ContentPage
+    {
+        public MH_Chao()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdchao_Clicked(object sender, EventArgs e)
+        {
+            string Hoten = txthoten.Text;
+            DisplayAlert("Chào", "Chào bạn: " + Hoten, "Thoát");
+
+        }
+    }
+}
